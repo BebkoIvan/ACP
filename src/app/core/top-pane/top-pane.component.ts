a21e8754f770561e97919c8ff9ae0764ce34144f
Ivan Bebko <bebkovan@gmail.com>
2019-05-17 11:55:45 +0300

    Search-bar is 100% width now

--- src/app/core/top-pane/top-pane.component.ts
+++ src/app/core/top-pane/top-pane.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from "@angular/core";
+import { Component, OnInit,EventEmitter,Input,Output, ErrorHandler } from "@angular/core";
 
 @Component({
     selector: "app-top-pane",
@@ -8,9 +8,6 @@ import { Component, OnInit, HostListener } from "@angular/core";
 export class TopPaneComponent implements OnInit {
     constructor() {}
     search_active: Boolean = false;
-    mobile: Boolean = false;
-    innerWidth: any;
-    menu_active: Boolean = false;
     profile: { user_name: string; get_img(): string } = {
         user_name: "Jeff Bezos",
         get_img() {
@@ -21,39 +18,16 @@ export class TopPaneComponent implements OnInit {
             );
         }
     };
-    ngOnInit() {
-        this.innerWidth = window.innerWidth;
-        if (this.innerWidth >= 576) {
-            this.mobile = false;
-        } else {
-            this.mobile = true;
-        }
+    @Input() menu_active:boolean;
+    @Output()menu_click = new EventEmitter();
+
+    menu_handler(e:Event):void{
+      this.menu_click.emit();
     }
-    @HostListener("window:resize", ["$event"]) onResize(event) {
-        if (
-            (this.innerWidth < 576 && window.innerWidth >= 576) ||
-            (this.innerWidth >= 576 && window.innerWidth < 576)
-        ) {
-            this.search_active = false;
-            this.menu_active = false;
-        }
-        this.innerWidth = window.innerWidth;
-        if (this.innerWidth >= 576) {
-            this.mobile = false;
-        } else {
-            this.mobile = true;
-        }
+  
+    ngOnInit() {
     }
     handleSearchClick(e: Event) {
         this.search_active = !this.search_active;
-    }
-    handleMenuClick(e: Event) {
-        this.menu_active = !this.menu_active;
-        let menu: HTMLElement = document.querySelector(".side-menu");
-        if (this.menu_active) {
-            menu.className += " menu-active";
-        } else {
-            menu.className = "side-menu";
-        }
     }
 }
