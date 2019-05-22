c343a891b8ca4a3fd2d4fc497bf52fd90e192a28
Ivan Bebko <bebkovan@gmail.com>
2019-05-23 01:21:47 +0300

    TimeStamp updated

--- src/app/shared/timestamp/timestamp.component.ts
+++ src/app/shared/timestamp/timestamp.component.ts
@@ -8,26 +8,9 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from "@angular/core
 })
 export class TimestampComponent implements OnInit {
     @Input() date: string;
-
-    dateFormatted: Date;
-    year: string;
-    month: string;
-    dt: string;
+    @Input() format:string;
 
     constructor() {}
 
-    ngOnInit() {
-        this.dateFormatted = new Date(this.date);
-        this.year = `${this.dateFormatted.getFullYear()}`;
-        this.month = `${this.dateFormatted.getMonth() + 1}`;
-        this.dt = `${this.dateFormatted.getDate()}`;
-
-        if (parseInt(this.dt) < 10) {
-            this.dt = "0" + this.dt;
-        }
-
-        if (parseInt(this.month) < 10) {
-            this.month = "0" + this.month;
-        }
-    }
+    ngOnInit() {}
 }
