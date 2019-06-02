4cc244a3495f1614588f889faacfe44e4d3668dd
Edgar0055 <bebkovan@gmail.com>
2019-06-02 23:32:29 +0300

    category ‘All’ is now selected by default

--- src/app/workshops/workshops-feed/workshops-feed.component.ts
+++ src/app/workshops/workshops-feed/workshops-feed.component.ts
@@ -1,104 +1,107 @@
-import { Component, OnInit,Input,HostListener } from "@angular/core";
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit,Input,HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserInfoService } from 'src/app/services/user-info.service';
 @Component({
-    selector: "app-workshops-feed",
-    templateUrl: "./workshops-feed.component.pug",
-    styleUrls: ["./workshops-feed.component.scss"],
+    selector: 'app-workshops-feed',
+    templateUrl: './workshops-feed.component.pug',
+    styleUrls: ['./workshops-feed.component.scss'],
 })
 export class WorkshopsFeedComponent implements OnInit {
 
-    scrollTop:number;
-    topPosToStartShowing:number = 150;
-    workshops:Workshop[];
-    categories:string[]=["All","My Workshops","Favorite"];
-    user:User;
+    scrollTop: number;
+    topPosToStartShowing = 150;
+    workshops: Workshop[];
+    categories: string[] = ['All', 'My Workshops', 'Favorite'];
+    user: User;
     tagsList: Array<Tag> = [
         {
-         tagTitle: "JavaScript",
+         tagTitle: 'JavaScript',
          isActive: false 
 
         },
 
         {
-            tagTitle: "Social",
+            tagTitle: 'Social',
             isActive: false 
    
            },
 
         {
-            tagTitle: "NodeJs",
+            tagTitle: 'NodeJs',
             isActive: false
         },
 
         {
-            tagTitle: "OOP",
+            tagTitle: 'OOP',
             isActive: false
         },
 
         {
-            tagTitle: "Angular",
+            tagTitle: 'Angular',
             isActive: false
         },
 
         {
-            tagTitle: "React",
+            tagTitle: 'React',
             isActive: false
         },
 
         {
-            tagTitle: "Vue",
+            tagTitle: 'Vue',
             isActive: false
         },
 
         {
-            tagTitle: "C++",
+            tagTitle: 'C++',
             isActive: false
         },
 
         {
-            tagTitle: "Java",
+            tagTitle: 'Java',
             isActive: false
         },
 
         {
-            tagTitle: "REST API",
+            tagTitle: 'REST API',
             isActive: false
         },
         {
-            tagTitle: "Pascal",
+            tagTitle: 'Pascal',
             isActive: false
         },
         {
-            tagTitle: "Scala",
+            tagTitle: 'Scala',
             isActive: false
         }
     ];
-    
-    constructor(private route:ActivatedRoute,private _user:UserInfoService) {}
 
+    constructor(private route: ActivatedRoute,private _user: UserInfoService, private router: Router) {
+        if (!route.snapshot.params.category) {
+            this.router.navigate([''], {
+                queryParams: {  category: 'All' },
+                queryParamsHandling: 'merge'
+            });
 
-    ngOnInit() {       
-        this.route.data.subscribe(data => this.workshops=data.workshops);
-        this.user=this._user.profile;
+        }
     }
 
-  
-    
-    @HostListener('scroll', ['$event'])onScroll($event:Event):void {
-            this.scrollTop=$event.srcElement.scrollTop;
-            
+
+    ngOnInit() {
+        this.route.data.subscribe(data => this.workshops=data.workshops);
+        this.user = this._user.profile;
+    }
+
+    @HostListener('scroll', ['$event'])onScroll($event: Event): void {
+            this.scrollTop = $event.srcElement.scrollTop;
     };
 
-    
-    scroll():void {
-        let el=document.getElementById("topel");
+    scroll(): void {
+        let el = document.getElementById('topel');
         el.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-            inline: "nearest"
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest'
         })
       }
- 
-    
+
 }
