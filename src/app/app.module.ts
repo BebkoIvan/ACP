96b41b82df5c5a9dd87f26ada6682acb79efeea5
Edgar0055 <bebkovan@gmail.com>
2019-06-04 19:14:55 +0300

    range directive created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from "./app.component";
 import {CoreModule} from "./core/core.module";
 import {SharedModule} from "./shared/shared.module";
 import { DateAgoPipe } from './pipes/date-ago.pipe';
+import { RangeDirective } from './directives/range.directive';
 
 
 
 
 @NgModule({
-    declarations: [AppComponent, DateAgoPipe],
+    declarations: [AppComponent, DateAgoPipe, RangeDirective],
     imports: [BrowserModule, AppRoutingModule,CoreModule,SharedModule],
     providers: [],
     bootstrap: [AppComponent]
