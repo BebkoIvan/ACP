0495d0c9b9f5d290c1d201c840f7eb9ebd9163f0
Edgar0055 <bebkovan@gmail.com>
2019-06-12 14:36:18 +0300

    added path to login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,37 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { LoginFormComponent } from './shared/login-form/login-form.component';
 
 const routes: Routes = [
     {
-        path: "",
-        redirectTo: "workshops",
-        pathMatch: "full"
+        path: '',
+        redirectTo: 'workshops',
+        pathMatch: 'full'
     },
 
     {
-        path: "workshops",
-        loadChildren: "./workshops/workshops.module#WorkshopsModule"
+        path: 'workshops',
+        loadChildren: './workshops/workshops.module#WorkshopsModule'
     },
 
     {
-        path:"dashboard",
-        loadChildren: "./dashboard/dashboard.module#DashboardModule"
+        path:'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
     },
 
     {
-        path:"quizzes",
-        loadChildren: "./quizzes/quizzes.module#QuizzesModule"
+        path:'quizzes',
+        loadChildren: './quizzes/quizzes.module#QuizzesModule'
     },
 
-    { 
+    {
+        path: 'login',
+        pathMatch: 'full',
+        component: LoginFormComponent
+    },
+
+    {
     path: '**',
     component: PageNotFoundComponent
     }
