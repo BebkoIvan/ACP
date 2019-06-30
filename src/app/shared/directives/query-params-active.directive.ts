0398a1e0bb091ecae1fac4708b66b1ea552ad73b
Edgar0055 <bebkovan@gmail.com>
2019-06-30 20:55:18 +0300

    removed hostbinding from query-params directive

--- src/app/shared/directives/query-params-active.directive.ts
+++ src/app/shared/directives/query-params-active.directive.ts
@@ -10,14 +10,15 @@ import {
     OnChanges
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Directive({
     selector: '[appQueryParamsActive]'
 })
-export class QueryParamsActiveDirective implements OnChanges {
+export class QueryParamsActiveDirective implements OnInit {
     
     @Input() tag;
-    @HostBinding('class.active') private isActive: boolean;
+    
 
     constructor(
         private route: ActivatedRoute,
@@ -26,21 +27,19 @@ export class QueryParamsActiveDirective implements OnChanges {
         private renderer2: Renderer2
     ) {}
 
-    ngOnChanges(): void {
+    ngOnInit(): void {
         this.route.queryParams.subscribe(params => {
             if (params.tags) {
                 if (params.tags.split(',').includes(this.tag.seq.toString())) {
                     this.renderer2.addClass(this.el.nativeElement, 'active');
-                    this.isActive = true;
                 } else {
                     this.renderer2.removeClass(this.el.nativeElement, 'active');
-                    this.isActive = false;
                 }
             } else {
                 this.renderer2.removeClass(this.el.nativeElement, 'active');
-                this.isActive = false;
             }
         });
     }
+    }
+        
 
-}
