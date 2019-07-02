38ff8777a0afe7e62aa68fc30c84d4ea468f3eae
Edgar0055 <bebkovan@gmail.com>
2019-07-02 23:14:13 +0300

    removed position relative from quizCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from './reducers';
+import { SignedIn } from './auth/store/auth.actions';
 
 @Component({
     selector: 'app-root',
@@ -10,11 +13,18 @@ export class AppComponent implements OnInit {
     title = 'ACP';
     menuActive: boolean = false;
 
-    constructor(public userService: AuthService) {}
+    constructor(public authService: AuthService, private store: Store<AppState>) {}
 
-    ngOnInit() { }
+    ngOnInit() {
+        const token = this.authService.getToken();
+        if (token) {
+        this.authService.getCurrentUser().subscribe( (data: AuthData) => {
+            this.store.dispatch(new SignedIn({authData: data}));
+        });
+    }
+}
 
-    handleMenuClick() {
+        handleMenuClick() {
         this.menuActive = !this.menuActive;
     }
 
